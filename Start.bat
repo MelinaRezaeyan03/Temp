923018196e97d9fa6087a37a1e6c91ab5493b841
MelinaRezaeyan03 <melina_rezaeyan@mathdep.iust.ac.ir>
2023-04-12 01:30:51 +0430

    Start.bat

--- Start.bat
+++
@@ -1,9 +0,0 @@
-path C:\Users\Rezaeyan\AppData\Local\GitHubDesktop\app-3.2.1\resources\app\git\cmd
-c:
-cd\Life\Temp
-git init 
-git add . 
-git commit -m "first commit" 
-git branch -M main 
-git remote add origin https://github.com/MelinaRezaeyan03/Temp.git
-git push origin main
